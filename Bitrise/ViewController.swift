7e8763ecd288da57cbb637eb4526a1f528cb7d24
Yitzchak Bernstein <yitzchak@coapp.co.il>
2017-10-08 12:37:02 +0300

    Initial Commit

---
+++ Bitrise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bitrise
+//
+//  Created by Yitzchak Bernstein on 10/8/17.
+//  Copyright © 2017 ElasticMedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
